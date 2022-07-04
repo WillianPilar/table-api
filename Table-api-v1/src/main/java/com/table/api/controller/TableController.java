4c1fc6ba7f3144a49d4611e9d524915098b14885
willianpilar <willianpilar@hotmail.com>
2022-07-03 23:13:40 -0300

    Swagger adicionado

--- Table-api-v1/src/main/java/com/table/api/controller/TableController.java
+++ Table-api-v1/src/main/java/com/table/api/controller/TableController.java
@@ -2,6 +2,8 @@ package com.table.api.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,23 +16,31 @@ import com.table.api.service.impl.TableServiceImpl;
 @RestController
 @RequestMapping(value = "/table/v1")
 public class TableController {
-	
+
 	private TableService tableService = new TableServiceImpl();
-	
+
 	@PostMapping("/process")
-	public int[][] tableProcessJson(@RequestBody EntryNumbersDTO entryNumbers) {
-		
+	public ResponseEntity<int[][]> tableProcessJson(@RequestBody EntryNumbersDTO entryNumbers) {
+
 		int[][] matrizProcess = new int[0][0];
-		
-		List<Integer> numbersList = tableService.processNumbersToList(entryNumbers.getNumbers().trim());
-		int[][] matriz = tableService.processListToMatriz(numbersList);
-		
-		if (matriz.length > 0) {
-			List<Integer> newList = tableService.moveNumbersInNewList(numbersList.size() , matriz);
-			matrizProcess = tableService.moveNumbersInMatriz(newList, matriz, numbersList.size());
+
+		try {
+
+			List<Integer> numbersList = tableService.processNumbersToList(entryNumbers.getNumbers().trim());
+			int[][] matriz = tableService.processListToMatriz(numbersList);
+
+			if (matriz.length > 0) {
+				List<Integer> newList = tableService.moveNumbersInNewList(numbersList.size(), matriz);
+				matrizProcess = tableService.moveNumbersInMatriz(newList, matriz, numbersList.size());
+			} else {
+				return new ResponseEntity<int[][]>(matrizProcess, HttpStatus.BAD_REQUEST);
+			}
+
+		} catch (Exception e) {
+			return new ResponseEntity<int[][]>(matrizProcess, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-		
-		return matrizProcess;
+
+		return ResponseEntity.ok(matrizProcess);
 	}
-	
+
 }
